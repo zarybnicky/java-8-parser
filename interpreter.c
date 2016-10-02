4e64de1009132c3dd7c7821742d23725be53e1a5
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2016-10-03 01:05:45 +0200

    Add file loading, comment out tests for the far future.

--- interpreter.c
+++ interpreter.c
@@ -12,6 +12,18 @@
 
 int main(int argc, char *argv[])
 {
+  if (argc != 2) {
+    fprintf(stderr, "Invalid arguments, usage: %s <FILENAME>\n", argv[0]);
+    return 99;
+  }
+  FILE *f = fopen(argv[1], "r");
+  if (f == NULL) {
+    perror("Error while opening file");
+    return 99;
+  }
+
   printf("Not implemented.");
+
+  fclose(f);
   return 0;
 }
