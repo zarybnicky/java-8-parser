e2de935895683de6d79960f4e16815a0d19f1702
xvasko14 <vaskomichal7@gmail.com>
2016-10-05 20:03:20 +0200

    Create scanner.c

---
+++ scanner.c
@@ -0,0 +1,10 @@
+/*
+ * Project: IFJ16, a programming language interpreter
+ * FIT VUT Brno
+ * Authors: xzaryb00 - Zarybnický Jakub
+ *          xtamas01 - Tamaškovič Marek
+ *          xvasko12 - Vaško Martin
+ *          xvasko14 - Vaško Michal
+ *          xzales12 - Záleský Jiří
+ */
+ // Pomaly na tom pracujem postupne tu zacnem pridavat co mam 
