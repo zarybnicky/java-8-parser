8f31cf3bc88c2856b39809b35b79356f06ea470e
Jirzales <jirzales@gmail.com>
2016-10-08 14:07:36 +0200

    Stringology

---
+++ Stringology.c
@@ -0,0 +1,11 @@
+/*
+ * Project: IFJ16, a programming language interpreter
+ * FIT VUT Brno
+ * Authors: xzaryb00 - Zarybnický Jakub
+ *          xtamas01 - Tamaškovič Marek
+ *          xvasko12 - Vaško Martin
+ *          xvasko14 - Vaško Michal
+ *          xzales12 - Záleský Jiří
+ */
+ 
+ 
