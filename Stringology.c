d0f55aefcf3438c7c199e87d96faed8c905a5bea
Jirzales <jirzales@gmail.com>
2016-10-08 14:30:34 +0200

    Update Stringology.c

--- Stringology.c
+++ Stringology.c
@@ -8,4 +8,14 @@
  *          xzales12 - Záleský Jiří
  */
  
+int readInt();
+double readDouble();
+String readString();
+void print();
+int length(String s);
+String substr(String s,int i, int n);
+int compare(String s1, String s2);
+int find(String s, String search);
+String sort(String s);
+
  
