a820d1afce3325b7df0b517e99b2fdad263bbe9f
xvasko14 <vaskomichal7@gmail.com>
2016-10-10 17:23:44 +0200

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -15,79 +15,79 @@
 int control_res_key_word(String str)
 {
     if (strlen(str) > LNG_KEYWORD)
-      return IDENTIFIER;
+      return IDEN;
 
     if (str[0] == 'b') {
         if (strcmp(str, "boolean") == 0)
             return BOOLEAN;
         if (strcmp(str, "break") == 0)
             return BREAK;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 'c') {
         if (strcmp(str, "class") == 0)
             return CLASS;
         if (strcmp(str, "continue") == 0)
             return CONTINUE;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 'd') {
         if (strcmp(str, "do") == 0)
             return DO;
         if (strcmp(str, "double") == 0)
             return DOUBLE;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 'e') {
         if (strcmp(str, "else") == 0)
             return ELSE;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 'f') {
         if (strcmp(str, "false") == 0)
             return FALSE;
         if (strcmp(str, "for") == 0)
             return FOR;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 'i') {
         if (strcmp(str, "if") == 0)
             return IF;
         if (strcmp(str, "int") == 0)
             return INT;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 'r') {
         if (strcmp(str, "return") == 0)
             return RETURN;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 'S') {
         if (strcmp(str, "String") == 0)
             return STRING;
-        return IDENTIFIER;
+        return IDEN;;
     }
      if (str[0] == 's') {
         if (strcmp(str, "static") == 0)
             return STATIC;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 't') {
         if (strcmp(str, "true") == 0)
             return TRUE;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 'w') {
         if (strcmp(str, "void") == 0)
             return VOID;
-        return IDENTIFIER;
+        return IDEN;;
     }
     if (str[0] == 'w') {
         if (strcmp(str, "while") == 0)
             return WHILE;
-        return IDENTIFIER;
+        return IDEN;;
     }
-    return IDENTIFIER;
+    return IDEN;;
 }
 
 int Get_Token(void)
