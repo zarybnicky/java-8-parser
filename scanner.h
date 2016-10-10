fac7101775aa85709a21e54b72167f738da413d1
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2016-10-10 14:15:01 +0200

    Edit scanner API

--- scanner.h
+++ scanner.h
@@ -13,24 +13,50 @@
 
 #include "types.h"
 #include <stdio.h>
+#include <string.h>
 
 typedef enum {
-    Integer, Double, string, Type, SimpleId, CompoundId, Reserved, Symbol
+    RESERVED,
+    ID_SIMPLE,
+    ID_COMPOUND,
+    LIT_INTEGER,
+    LIT_STRING,
+    LIT_BOOLEAN,
+    LIT_DOUBLE,
 } TokenType;
+
+typedef enum {
+    RES_BOOLEAN,
+    RES_BREAK,
+    RES_CLASS,
+    RES_CONTINUE,
+    RES_DO,
+    RES_DOUBLE,
+    RES_ELSE,
+    RES_FALSE,
+    RES_FOR,
+    RES_IF,
+    RES_INT,
+    RES_RETURN,
+    RES_STRING,
+    RES_STATIC,
+    RES_TRUE,
+    RES_VOID,
+    RES_WHILE,
+} ReservedWord;
 
 typedef struct {
     int lineNum;
     int lineChar; //For error reporting
     TokenType type;
     union {
-        struct { int intValue; };
-        struct { double doubleValue; };
-        struct { char *stringValue; };
-        struct { char *typeValue; };
-        struct { char *localId; };
-        struct { char *namespace; char *staticId; };
-        struct { char *reservedValue; };
-        struct { char *symbolValue; };
+        struct { ReservedWord reserved; };           //RESREVED
+        struct { char *localId; };                   //ID_SIMPLE
+        struct { char *namespace; char *staticId; }; //ID_COMPOUND
+        struct { int intValue; };                    //LIT_INTEGER
+        struct { double doubleValue; };              //LIT_DOUBLE
+        struct { char boolValue; };                  //LIT_BOOLEAN
+        struct { char *stringValue; };               //LIT_STRING
     };
 } Token;
 
