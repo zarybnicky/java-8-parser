86b9c3759bbeefe6175318fab0f82eb8ffdc5d51
Matovidlo <Matovidlo2@gmail.com>
2016-10-10 16:47:28 +0200

    mfcmit

---
+++ hlavicka.c
@@ -0,0 +1,13 @@
+/*
+	IFJ16 = JavaSE 8.0 interpret
+
+	zloženie týmu:
+	Martin Vaško = xvasko12
+	Michal Vaško = xvasko14
+	Jakub Zarybnický = xzaryb00
+	Jiří Záleský = xzales00
+	Marek Tamaškovič = xtamas00
+
+	*Mozete túto hlavičku upraviť aby bola jednotná notácia vo všetkých kódoch
+
+*/
