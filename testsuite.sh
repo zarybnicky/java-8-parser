3f1a39b42776b43766d891f3d3d6a5cbdb58ef8e
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2016-11-07 16:01:14 +0100

    Enable more tests

--- testsuite.sh
+++ testsuite.sh
@@ -4,9 +4,9 @@
 
 assert_raises "./ifj16 test/1_unknown_char.java" 1
 assert_raises "./ifj16 test/2_no_class.java" 2
-#assert_raises "./ifj16 test/3_class_redefinition.java" 3
-#assert_raises "./ifj16 test/3_ifj16_redefinition.java" 3
-#assert_raises "./ifj16 test/4_wrong_param_number.java" 4
+assert_raises "./ifj16 test/3_class_redefinition.java" 3
+assert_raises "./ifj16 test/3_ifj16_redefinition.java" 3
+assert_raises "./ifj16 test/4_wrong_param_number.java" 4
 #assert_raises "./ifj16 test/8_undefined_variable.java" 8
 #assert_raises "./ifj16 test/9_division_by_zero.java" 9
 #assert_raises "./ifj16 test/10_substr_start_over.java" 10
