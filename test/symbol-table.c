2ede0f45e01d5f18fd455cc649081361662052cb
Matovidlo <Matovidlo2@gmail.com>
2016-11-18 17:56:49 +0100

    Cannot use static char*, repair tests

--- test/symbol-table.c
+++ test/symbol-table.c
@@ -19,7 +19,9 @@ void pre_order(Node *node){
 }
 
 int main(){
-  char name[10] ="a";
+  char *name;
+  name = malloc (4 * sizeof(char));
+  strcpy(name,"h");
   Value *new_val = createValue (1);
   new_val->data.integer = 10;
   printValue (new_val);
@@ -33,42 +35,75 @@ int main(){
   new_v->data.integer = 10;
   // TODO same value free problem!! ir.c
   Node *new_nd = NULL;
-  new_nd = createValueNode ("b",new_v);
+
+  char *string;
+  string = malloc (4 * sizeof(char));
+
+  strcpy(string,"g");
+  new_nd = createValueNode (string,new_v);
 //  printNode(new_nd);
 
   SymbolTable *new_table = createSymbolTable();
   Node *lookup = NULL;
 
   Value *va = createValue (1);
-  va->data.integer = 10;
+  va->data.integer = 150;
   Node *a = NULL;
-  a = createValueNode ("c",va);
+
+  char *string2;
+  string2 = malloc (4* sizeof(char));
+  strcpy(string2, "a");
+  a = createValueNode (string2,va);
 
   Value *vb = createValue (1);
-  vb->data.integer = 10;
+  vb->data.integer = 40;
   Node *b = NULL;
-  b = createValueNode ("d",vb);
+
+  char * string3;
+  string3 = malloc (4* sizeof(char));
+  strcpy(string3,"b");
+  b = createValueNode (string3,vb);
 
   Value *vc = createValue (1);
-  vc->data.integer = 10;
+  vc->data.integer = 30;
   Node *c = NULL;
-  c = createValueNode ("e",vc);
+
+  char *string4;
+  string4 = malloc (4* sizeof(char));
+  string4[0] = 'c';
+  string4[1] = '\0';
+
+  c = createValueNode (string4,vc);
 
   Value *vd = createValue (1);
-  vd->data.integer = 10;
+  vd->data.integer = 20;
   Node *d = NULL;
-  d = createValueNode ("f",vd);
+
+  char *string5;
+  string5 = malloc (4 * sizeof(char));
+  strcpy(string5,"d");
+  d = createValueNode (string5,vd);
 
 
   Value *ve = createValue (1);
-  ve->data.integer = 10;
+  ve->data.integer = 15;
   Node *e = NULL;
-  e = createValueNode ("i",ve);
+
+  char *string6;
+  string6 = (char *) malloc (4 * sizeof(char));
+  if (string6 != NULL){
+    strcpy(string6,"e");
+  }
+  e = createValueNode (string6,ve);
 
   Value *vf = createValue (1);
-  vf->data.integer = 10;
+  vf->data.integer = 11;
   Node *f = NULL;
-  f = createValueNode ("j",vf);
+
+  char *string7;
+  string7 = malloc (4 * sizeof(char));
+  strcpy(string7,"f");
+  f = createValueNode (string7,vf);
 
   table_insert (new_table, new_node);
   table_insert (new_table, new_nd);
