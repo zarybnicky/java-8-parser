1e9bd5c07a4137673496ccab0aeea5edbda78482
xvasko14 <vaskomichal7@gmail.com>
2016-12-11 23:20:31 +0100

    Add base hex test

---
+++ test/0_base_floathex_test.java
@@ -0,0 +1,16 @@
+class Main{
+    static void run(){
+
+        double a = 0xff.ffp-3;
+        double b = 05432;
+
+        if(a < 0){
+            b = 42;
+        }
+        else{
+            b = 99;
+        }
+
+        ifj16.print(b + b"\n");
+    }
+
