31a38a9da762dabeba9486e77efc3d3e81568fbb
xvasko14 <vaskomichal7@gmail.com>
2016-12-11 20:57:28 +0100

    Test for BASE

---
+++ test/0_base_test.java
@@ -0,0 +1,16 @@
+class Main{
+    static void run(){
+
+        double a = 0xff;
+        double b = 0b101010111101;
+
+        if(a > 0){
+            b = 6;
+        }
+        else{
+            b = 8;
+        }
+
+        ifj16.print(b +"\n");
+    }
+}
